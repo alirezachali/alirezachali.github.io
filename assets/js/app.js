bb2949a5692211b1f41696b0e20ebd13e55f226e
Alireza Chali <mr.alirezachali@gmail.com>
2026-07-25 00:45:02 +0300

    کامل شدن نسخه ابتدایی و خام پروژه

---
+++ assets/js/app.js
@@ -0,0 +1,109 @@
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".custom-navbar");
+
+    if (window.scrollY > 40) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
+
+new Typed("#typing",{
+
+    strings:[
+
+        "Full Stack Developer",
+
+        "Laravel Developer",
+
+        "PHP Programmer",
+
+        "Backend Engineer"
+
+    ],
+
+    typeSpeed:70,
+
+    backSpeed:40,
+
+    backDelay:1800,
+
+    loop:true
+
+});
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        if (scrollY >= sectionTop) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") == "#" + current) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
+
+
+
+const cards = document.querySelectorAll(".project-card");
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity = "1";
+
+            entry.target.style.transform = "translateY(0)";
+
+            entry.target.style.transition = ".6s ease";
+
+        }
+
+    });
+
+});
+
+cards.forEach(card=>observer.observe(card));
+
+
+document.querySelectorAll(".project-card").forEach(card=>{
+
+    card.addEventListener("mousemove",e=>{
+
+        const rect=card.getBoundingClientRect();
+
+        card.style.setProperty("--x",(e.clientX-rect.left)+"px");
+
+        card.style.setProperty("--y",(e.clientY-rect.top)+"px");
+
+    });
+
+});
